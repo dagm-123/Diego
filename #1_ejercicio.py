91a67aea4a0f33fc71062a4a08679c82f4414f5b
Diego <diegogalino17@gmail.com>
2025-02-22 17:15:48 -0400

    ejercicio_1 #1_ejercicio.py

---
+++ #1_ejercicio.py
@@ -0,0 +1,7 @@
+numero1 = (20,40)
+numero2 = (30,30)
+
+print("El resultado de la multiplicacion es : ", numero1[0] * numero2[0])
+
+
+print("El resultado de la suma es : ", numero1[1] + numero2[1]) 
